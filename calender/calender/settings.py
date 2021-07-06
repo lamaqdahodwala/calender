7e5455b8bf0566602d930849f4b0bc2ccf49e10f
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-07-06 13:28:15 -0400

    added url conf

--- calender/calender/settings.py
+++ calender/calender/settings.py
@@ -119,8 +119,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [BASE_DIR.joinpath('staticfiles')]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
