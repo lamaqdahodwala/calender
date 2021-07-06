1d9805101fba272e291f5d63f08b267aa4e72230
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-07-06 13:32:45 -0400

    edited settings

--- calender/calender/settings.py
+++ calender/calender/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main.apps.MainConfig',
+    'django_svelte'
 ]
 
 MIDDLEWARE = [
@@ -119,6 +121,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    str(BASE_DIR.parent / 'svelte' / 'public' / 'build')
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
