dcbf65bc363acf604c06b0ed4495a0b8d6854dda
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-07-06 13:26:26 -0400

    modified settings

--- calender/calender/settings.py
+++ calender/calender/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'calender.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR.joinpath('templates'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [BASE_DIR.joinpath('staticfiles')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
